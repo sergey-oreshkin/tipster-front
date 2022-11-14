099f6864ba763854ec91b17f7c8b953e8ba997b2
sergey-oreshkin <joknife79@ya.ru>
2022-11-14 11:39:53 +0500

    feat: header

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import cl from './Logo.module.css';
+
+const Logo = () => {
+    return (
+        <div className={cl.logo}>
+            <h2>Tipster</h2>
+        </div>
+    )
+}
+
+export default Logo;
