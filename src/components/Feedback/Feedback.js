5679ac46187b3e7aa0e60228399a966ed6c9163f
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-12-27 22:57:54 +0400

    added logo

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -8,7 +8,7 @@ const Feedback = () => {
       <div className='feedback__section'>
         <div className='feedback__box'>
           <h2 className='feedback__title'>Хотите связаться с нами?</h2>
-          <p className='feedback__subtitle'>Если хотите сообщить об ошибке или предложить новую функциональность, заполните форму обратной связи.</p>
+          <p className='feedback__subtitle'>Чтобы сообщить об ошибке или предложить новую функциональность, заполните форму обратной связи.</p>
           <button className='feedback__button navigation__button-link navigation__button-link_color link button'>Написать</button>
         </div>
         <img className='feedback__picture' alt='изображение с громкоговорителем и сообщением об ошибке' src={feedbackPic}></img>
