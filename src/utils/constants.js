98af20c16b5980f5dbff66ead56968d2cbc4cdbd
sergey-oreshkin <joknife79@ya.ru>
2023-03-30 10:54:00 +0500

    fix constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,33 +1,30 @@
 
-// const host = window.location.hostname;
-// const protocol = window.location.protocol;
-// const port = window.location.port;
-
-// export const apiPrefix = '/api/v0';
-// export const hostUrl = protocol + '//' + host + ':' + port;
-// export const googleLoginUri = '/oauth2/authorize/google';
-// export const githubLoginUri = '/oauth2/authorize/github';
-
-// export const userProfileUri = '/users/profile';
-// export const themesUri = '/themes';
-// export const tipsUri = '/tips';
-
-// export const tokenStorageName = 'BaererToken';
-
-
-// // const host = window.location.hostname;
-// // const protocol = window.location.protocol;
-// // const port = window.location.port;
+const host = window.location.hostname;
+const protocol = window.location.protocol;
+const port = window.location.port;
 
 export const apiPrefix = '/api/v0';
-export const hostUrl = 'http://localhost:8080';
-// export const hostUrl = protocol + '//' + host + ':' + port;
-export const googleLoginUri = 'http://localhost:8080/oauth2/authorize/google';
-export const githubLoginUri = 'http://localhost:8080/oauth2/authorize/github';
+export const hostUrl = protocol + '//' + host + ':' + port;
+export const googleLoginUri = '/oauth2/authorize/google';
+export const githubLoginUri = '/oauth2/authorize/github';
 
 export const userProfileUri = '/users/profile';
-
 export const themesUri = '/themes';
 export const tipsUri = '/tips';
 
 export const tokenStorageName = 'BaererToken';
+
+
+
+// export const apiPrefix = '/api/v0';
+// export const hostUrl = 'http://localhost:8080';
+// // export const hostUrl = protocol + '//' + host + ':' + port;
+// export const googleLoginUri = 'http://localhost:8080/oauth2/authorize/google';
+// export const githubLoginUri = 'http://localhost:8080/oauth2/authorize/github';
+
+// export const userProfileUri = '/users/profile';
+
+// export const themesUri = '/themes';
+// export const tipsUri = '/tips';
+
+// export const tokenStorageName = 'BaererToken';
